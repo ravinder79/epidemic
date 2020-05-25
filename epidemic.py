5e07e6981c94cb47f1ad49f8152e0c794cd18227
Ravinder Singh <ravinder79@gmail.com>
2020-05-25 17:54:29 -0500

    added day label

--- epidemic.py
+++ epidemic.py
@@ -6,7 +6,10 @@ import matplotlib.animation as animation
 from scipy.spatial.distance import pdist, squareform
 import matplotlib.pyplot as plt
 
+
+
 #------------------------------------------------------------
+global day
 #create a figure object and axes
 fig = plt.figure()
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
@@ -23,29 +26,37 @@ rect = plt.Rectangle(bounds[::2],
                      ec='none', lw=2, fc='none')
 ax.add_patch(rect)
 
+rect1 = plt.Rectangle((0.2, 0.75), 0.4, 0.15, ec = 'none', alpha=0.3, label = 'day', fc = 'none')
+ax.add_patch(rect1)
 
-n = 50
+n = 100
 person = np.ones(n, dtype=[('position', float, 2), ('velocity', float, 2),
                                       ('status',    float, 1),('size',    float, 1)])
 person['position'] = np.random.uniform(-2,2, size = (n, 2))
 #person['position'] = -0.5 + np.random.random((n, 2)) 
-person['velocity'] = -0.5 + np.random.random((n, 2))
+person['velocity'] = (-0.5 + np.random.random((n, 2))) * 1.5
 person['status'][0] = 0
-person['size'] = 0.04
-categories = np.array([1] * 50)
-categories[0] = 0.00
 
+person['size'] = 0.04
+categories = np.array([1] * n)
+categories[0] = 0.00
+day = 0
 cmap = np.array(['r', 'g'])
 size = 20
 #color_data = categories
 # Construct the scatter plot which we will update during animation
 scat = ax.scatter(person['position'][:,0], person['position'][:,1],
                   lw=0.5,  c= person['status'], s = 30, norm = plt.Normalize(vmin=0, vmax=1),
-            cmap = "bwr_r")
+            cmap = "bwr_r", label = 'day')
+legend = ax.legend()
 
 
 def update(frame_number):
-    global categories, rect, dt, ax, fig, colormap
+    
+    
+    global categories, rect, dt, ax, fig, colormap, legend
+    day = int(frame_number/30)
+    
     dt = 1 / 30 # 30fps
     person['position'] += dt * person['velocity']
 
@@ -76,13 +87,16 @@ def update(frame_number):
     person['velocity'][crossed_x1 | crossed_x2, 0] *= -1
     person['velocity'][crossed_y1 | crossed_y2, 1] *= -1
     rect.set_edgecolor('k')
-    #scat.set_offsets(categories)
-    #cmap = np.array(['r', 'g'])
-    #norm = plt.colors.Normalize(vmin=0, vmax=1)
+   
     scat.set_array(person['status'])
-    #colormap = np.array(['r', 'g'])
+    scat.set_label(day)
+    legend.remove()
+    legend = plt.legend( loc='upper left')
+    # red_patch = mpatches.Patch( label='day')
+    # plt.legend(handles=[red_patch])
+    
 
-    return scat,
+    return scat, [legend]
 # Construct the animation, using the update function as the animation
 # director.
 animation = animation.FuncAnimation(fig, update, interval=20)
