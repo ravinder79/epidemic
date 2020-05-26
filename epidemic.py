7fd58e1d87061f563e18b951cdb6a97a6ab2f5ea
Ravinder Singh <ravinder79@gmail.com>
2020-05-25 23:40:02 -0500

    blinking points

--- epidemic.py
+++ epidemic.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 
 
 #------------------------------------------------------------
-global day
+global day, s
 #create a figure object and axes
 fig = plt.figure()
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
+                     xlim=(-33.2, 33.2), ylim=(-32.4, 32.4))
 
 # set bounds for the box
-bounds = [-2, 2, -2, 2]
+bounds = [-20, 20, -20, 20]
 
 # rect is the box edge
 rect = plt.Rectangle(bounds[::2],
@@ -31,35 +31,45 @@ ax.add_patch(rect1)
 
 n = 100
 person = np.ones(n, dtype=[('position', float, 2), ('velocity', float, 2),
-                                      ('status',    float, 1),('size',    float, 1)])
-person['position'] = np.random.uniform(-2,2, size = (n, 2))
+                                      ('status',    float, 1),('size',    float, 1),('growth',    float, 1),('color',    float, 4)])
+person['position'] = np.random.uniform(-10,10, size = (n, 2))
 #person['position'] = -0.5 + np.random.random((n, 2)) 
-person['velocity'] = (-0.5 + np.random.random((n, 2))) * 1.5
+person['velocity'] = (-0.5 + np.random.random((n, 2))) * 10
 person['status'][0] = 0
-
-person['size'] = 0.04
-categories = np.array([1] * n)
-categories[0] = 0.00
+person['growth'] = np.random.uniform(1, 10, n)
+person['color'] = np.zeros((n,4))
+person['color'][:,1] = 0.5
+# person['color'][:,3] = 0
+# categories = np.array([1] * n)
+# categories[0] = 0.00
 day = 0
-cmap = np.array(['r', 'g'])
-size = 20
+#cmap = np.array(['r', 'g'])
+s= np.ones((n)) * 10
 #color_data = categories
 # Construct the scatter plot which we will update during animation
+# scat = ax.scatter(person['position'][:,0], person['position'][:,1],
+#                   lw=0.5,  c= person['status'], s = person['size'], norm = plt.Normalize(vmin=0, vmax=1),
+#             cmap = "bwr_r", label = 'day', alpha = 0.7, edgecolors= person['color'],facecolors='none')
+
 scat = ax.scatter(person['position'][:,0], person['position'][:,1],
-                  lw=0.5,  c= person['status'], s = 30, norm = plt.Normalize(vmin=0, vmax=1),
-            cmap = "bwr_r", label = 'day')
+        lw=0.5, s = person['size'], norm = plt.Normalize(vmin=0, vmax=1)
+            , label = 'day', alpha = 0.7, edgecolors= person['color'],facecolors='none')
 legend = ax.legend()
 
 
 def update(frame_number):
     
-    
-    global categories, rect, dt, ax, fig, colormap, legend
+    current_index = frame_number % n
+
+    global categories, rect, dt, ax, fig, colormap, legend, s
     day = int(frame_number/30)
     
     dt = 1 / 30 # 30fps
     person['position'] += dt * person['velocity']
-
+    person['color'][:, 3] =  (1.0/50) + person['color'][:, 3]
+    # print(person['color'][1])
+    person['color'][:,3] = np.clip(person['color'][:,3], 0, 1)
+    
     scat.set_offsets(person['position'])
     # check for crossing boundary
    
@@ -70,7 +80,9 @@ def update(frame_number):
 
     # find pairs of particles undergoing a collision
     D = squareform(pdist(person['position']))
-    ind1, ind2 = np.where(D < (2 * person['size']))
+    #ind1, ind2 = np.where(D < (2 * person['size']))
+    ind1, ind2 = np.where(D < (1))
+
     unique = (ind1 < ind2)
     ind1 = ind1[unique]
     ind2 = ind2[unique]
@@ -78,20 +90,39 @@ def update(frame_number):
     for i1, i2 in zip(ind1, ind2):
         if person['status'][i1] == 0:
             person['status'][i2] = 0
+            person['color'][i2][0] = 1
+           
+            person['color'][i2][1] = 0
         if person['status'][i2] == 0:
             person['status'][i1] = 0
+            person['color'][i1][0] = 1
+            person['color'][i1][1] = 0
 
-    for i in range (0, n):
-        categories[i] = person['status'][i]
-   
+
+    # for i in range(0,n):
+    #     if person['status'][i] == 0:
+    #         s[i] +=  2
+    #     if s[i] >=40:
+    #         s[i] = 20
+    
+    
+    s = np.where(person['status'] ==0, s+8, s)
+    s = np.where(s > 160, 20, s)
+    #print(person['status'])
+    print(s)
+    person['color'][:, 3] = (s-20)/140
+    print(person['color'])
     person['velocity'][crossed_x1 | crossed_x2, 0] *= -1
     person['velocity'][crossed_y1 | crossed_y2, 1] *= -1
     rect.set_edgecolor('k')
    
-    scat.set_array(person['status'])
+    scat.set_edgecolors(person['color'])
     scat.set_label(day)
     legend.remove()
     legend = plt.legend( loc='upper left')
+    scat.set_sizes(s)
+    #scat.set_sizes(person['size'])
+    
     # red_patch = mpatches.Patch( label='day')
     # plt.legend(handles=[red_patch])
     
