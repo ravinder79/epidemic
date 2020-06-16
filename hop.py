b271944c9e37f23547d332171b3a5d24408555e8
Ravinder Singh <ravinder79@gmail.com>
2020-06-15 21:02:12 -0500

    added function

--- hop.py
+++ hop.py
@@ -13,20 +13,33 @@ import math
 def hop(x0, x1):
 
     if x1 > 0:
+        
+        if abs(x0-x1) <= 2:
+            x1 = 0.5
+            return x1
 
-        if abs(x0-x1) <= 10:
+        if abs(x0-x1) <= 5:
             x1 = x1 - 2
+        # if x1 <4:
+        #     x1 = x0
+        #     return x1
         else:
-            x1 = x1 - 4
+            x1 = x1 - 5
     
         return x1
 
     if x1 < 0:
+        if abs(x0-x1) <= 2:
+            x1 = 0.5
+            return x1
 
-        if abs(x0-x1) <= 10:
-            x1 = x1 + 2
+        if abs(x0-x1) <= 5:
+            x1 = x1 + 1
+        # if x1 > -4:
+        #     x1 = x0
+        #     return x1
         else:
-            x1 = x1 + 4
+            x1 = x1 + 5
 
         return x1
 
@@ -77,19 +90,23 @@ def linehop(x0,y0, x1, y1):
 
 def linehop_q(x0,y0,x1,y1):
     d = math.sqrt((x0-x1)**2 + (y0-y1)**2)
-    if d >20:
-        d1 = d-20
-        
+    if d >15:
+        d1 = d-30
+    else:
+        d1 = d-15
+    
+    if abs(x0 > x1):
         x = x0 - d1 *(x0 - x1)/d
-        y = y0 - d1 *(y0 - y1)/d
-        return x, y
-        
-    if d <=20:
-        d1 = d-4
-        x = x0 - d1 *(x0 - x1)/d
-        y = y0 - d1 *(y0 - y1)/d
-        return x, y
+    
+    else:
+        x = x1
 
+    if y1 < y0:
+        y = y0 - d1 *(y0 - y1)/d
+    else:
+        y = y1
+
+    return x, y
 
 
 
